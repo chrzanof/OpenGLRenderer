17507961ed410a2ef9d596acccf9ee4e1d95209d
chrzanof <58990661+chrzanof@users.noreply.github.com>
2025-08-14 14:16:21 +0200

    cleared project

--- OpenGLRenderer/src/main.cpp
+++ OpenGLRenderer/src/main.cpp
@@ -3,10 +3,6 @@
 
 #define STB_IMAGE_IMPLEMENTATION
 #include "stb_image.h"
-#include "Shader.h"
-#include "VAO.h"
-#include "VBO.h"
-#include "EBO.h"
 #include <iostream>
 #include <vector>
 
@@ -49,80 +45,12 @@ int main()
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
 
-	Shader shader("src/shader.vs", "src/shader.fs");
-
-	
-	std::vector<float> vertices = {
-		// positions          // colors           // texture coords
-		 0.5f,  0.5f, 0.0f,   1.0f, 0.0f, 0.0f,   1.0f, 1.0f,   // top right
-		 0.5f, -0.5f, 0.0f,   0.0f, 1.0f, 0.0f,   1.0f, 0.0f,   // bottom right
-		-0.5f, -0.5f, 0.0f,   0.0f, 0.0f, 1.0f,   0.0f, 0.0f,   // bottom left
-		-0.5f,  0.5f, 0.0f,   1.0f, 1.0f, 0.0f,   0.0f, 1.0f    // top left
-
-	};
-	std::vector<unsigned int> indices = {
-		0, 1, 3,   // first triangle
-		1, 2, 3    // second triangle
-	};
-
-	// generujemy obiekty
-	
-	VAO vao{};
-	VBO vbo{};
-	EBO ebo{};
-	
-	
-	vao.Bind();
-	
-	vbo.Bind();
-	vbo.SetData(vertices);
-	ebo.Bind();
-	ebo.SetData(indices);
-
-	vao.SetAttributePointer(0, 3, GL_FLOAT, 8, 0);
-	vao.SetAttributePointer(1, 3, GL_FLOAT, 8, 3);
-	vao.SetAttributePointer(2, 2, GL_FLOAT, 8, 6);
-
-	unsigned int texture1;
-	glGenTextures(1, &texture1);
-	glActiveTexture(GL_TEXTURE0);
-	glBindTexture(GL_TEXTURE_2D, texture1);
-
-	stbi_set_flip_vertically_on_load(true);
-
-	int width, height, nrChannels;
-	unsigned char* data = stbi_load("src/container.jpg", &width, &height, &nrChannels, 0);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0,GL_RGB ,GL_UNSIGNED_BYTE, data);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	stbi_image_free(data);
-
-	unsigned int texture2;
-	glGenTextures(1, &texture2);
-	glActiveTexture(GL_TEXTURE1);
-	glBindTexture(GL_TEXTURE_2D, texture2);
-
-	data = stbi_load("src/awesomeface.png", &width, &height, &nrChannels, 0);
-
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-	glGenerateMipmap(GL_TEXTURE_2D);
-	stbi_image_free(data);
-	shader.use();
-	shader.setInt("texture1", 0);
-	shader.setInt("texture2", 1);
 	// pêtla renderingu
 	while (!glfwWindowShouldClose(window))
 	{
 		processInput(window);
+		glClearColor(0.5, 0.5, 0.5, 1);
 
-		glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		shader.use();
-		// glBindTexture(GL_TEXTURE_2D, texture);
-		vao.Bind();
-		// glDrawArrays(GL_TRIANGLES, 0, 3);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
